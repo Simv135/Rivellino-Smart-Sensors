b32cd812581cfe41ede2b79c6e2ba55735075694
Simv135 <109431365+Simv135@users.noreply.github.com>
2025-04-11 16:00:28 +0200

    Update moduloA.ino

--- moduloA/moduloA.ino
+++ moduloA/moduloA.ino
@@ -2,15 +2,20 @@
 #include <Arduino_HS300x.h>
 #include <math.h>  // Per isnan()
 
+//MISURA VIBRAZIONI
 float x, y, z;
 float vibration;
 float lastVibration;
 bool firstRead = true;
 const float soglia = 0.03;
-
+//MISURA FREQUENZA VIBRAZIONI
 unsigned long lastTime = 0;
 float frequency = 0.0;
 const unsigned long minInterval = 50;  // Minimo intervallo tra due rilevamenti per 20 Hz
+
+//TIMER TEMPERATURA E UMIDITA'
+unsigned long lastEnvRead = 0;
+const unsigned long envInterval = 5000;  // misura temperatura ogni 5 secondi
 
 void setup() {
   Serial.begin(115200);
@@ -19,23 +24,33 @@ void setup() {
   if (!IMU.begin()) {
     while (1);
   }
+  HS300x.begin();  // Inizializza il sensore di temperatura/umidità
 }
 
 void loop() {
+  //LETTURA VIBRAZIONI
   float vib = accelerometer();
   if (!isnan(vib) && frequency != 0.00 && frequency != 20.00) {
-    Serial.print("aa");          // Metri al secondo quadro
-    Serial.print(vib, 2);           // m/s2
-    Serial.print("ab");          // Frequenza oscillazioni
-    Serial.print(frequency, 2);   //  Hz
+    Serial.print("aa");
+    Serial.print(vib, 2);           // m/s²
+    Serial.print("ab");
+    Serial.print(frequency, 2);     // Hz
   }
-  Serial.print("ac");
-  Serial.print(HS300x.readTemperature()); 
-  Serial.print("ad");
-  Serial.print(HS300x.readHumidity()); 
-  Serial.println();
+
+  //LETTURA TEMPERATURA
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastEnvRead >= envInterval) {
+    lastEnvRead = currentMillis;
+
+    Serial.print("ac");
+    Serial.print(HS300x.readTemperature()); // °C
+    Serial.print("ad");
+    Serial.print(HS300x.readHumidity());    // %
+    Serial.println();
+  }
 }
 
+//ACCELLEROMETRO MISURA VIBRAZIONI
 float accelerometer() {
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
@@ -48,20 +63,18 @@ float accelerometer() {
       unsigned long deltaTime = currentTime - lastTime;
       
       if (!firstRead) {
-        // Se il deltaTime è maggiore di minInterval (50 ms per 20 Hz), calcola la frequenza
         if (deltaTime >= minInterval) {
-          frequency = 1000.0 / deltaTime;  // Converti in Hertz (ms → s)
+          frequency = 1000.0 / deltaTime;  // Hz
         } else {
-          // Se il tempo tra due letture è troppo breve, ignoriamo il dato
           return NAN;
         }
       }
-      
+
       lastTime = currentTime;
       lastVibration = vibration;
       firstRead = false;
-      return vibration;  // La vibrazione è ora espressa in m/s²
+      return vibration;
     }
   }
-  return NAN;  // Non ritorna nulla se la soglia non è superata
+  return NAN;
 }
